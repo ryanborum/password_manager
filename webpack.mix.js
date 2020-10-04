1672bff0678fd9c8431c0d96d03e2521106a8840
Ryan Borum <37112354+ryanborum@users.noreply.github.com>
2020-10-03 21:37:36 -0400

    Initial Bulma 0.9.0 Install

--- webpack.mix.js
+++ webpack.mix.js
@@ -11,5 +11,5 @@ const mix = require('laravel-mix');
  |
  */
 
-mix.js('resources/js/app.js', 'public/js')
+mix.sass('resources/sass/bulma.sass', 'public/css')
     .sass('resources/sass/app.scss', 'public/css');
